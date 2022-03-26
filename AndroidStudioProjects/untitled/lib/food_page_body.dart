d16be8ec852f59b96f9855e2825d9226d791fe49
buupass <buupass@buupasss-MacBook-Pro.local>
2022-03-26 23:51:36 +0300

    added a dot indicator dependancy

--- AndroidStudioProjects/untitled/lib/food_page_body.dart
+++ AndroidStudioProjects/untitled/lib/food_page_body.dart
@@ -15,8 +15,6 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var  _CurPagevalue= 0.0;
-  double _scalefactor = 0.8;
-
   @override
   void initstate(){
         super.initState();
@@ -50,91 +48,96 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
   Widget _buildpageitem(int index){
     Matrix4  matrix = new Matrix4.identity();
-    if (index==_CurPagevalue.floor()){
-      var CurrScale = 1-(_CurPagevalue - index) * 1-(_scalefactor);
-      matrix = Matrix4.diagonal3Values(1, CurrScale, 1);
-    }else if(index==_CurPagevalue.floor()+1){
-      var CurrScale = _scalefactor+(_CurPagevalue - index+1) * (1- _scalefactor);
-      matrix = Matrix4.diagonal3Values(1, CurrScale, 1);
+    /*if (index==_CurPagevalue.floor()){
+      var CurrScale =
 
-    }
-    return Transform(
-      transform: matrix,
-      child: Stack(
-        children: [
-          Container(
-            height: 220,
-            margin: EdgeInsets.only(left: 10, right: 10),
+    }*/
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color:index.isEven? Colors.red: Colors.yellow,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                      ""
+
+                  )
+              )
+
+
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 150,
+            margin: EdgeInsets.only(left: 50, right: 40, bottom: 10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color:index.isEven? Colors.red: Colors.yellow,
-                //color: Colors.yellow,
-               /* image: DecorationImage(
-                    fit: BoxFit.cover,
+                color:index.isEven? Colors.white: Colors.yellow,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5.0,
 
-                )*/
 
+                )
+
+              ]
 
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
             child: Container(
-              height: 150,
-              margin: EdgeInsets.only(left: 50, right: 40, bottom: 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color:index.isEven? Colors.white: Colors.yellow,
+              padding: EdgeInsets.only(top: 15, right: 15, bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side"),
+                  SizedBox(height: 10,),
+                  SizedBox(height: 20,),
 
-              ),
-              child: Container(
-                padding: EdgeInsets.only(top: 15, right: 15, bottom: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    BigText(text: "Chinese Side"),
-                    SizedBox(height: 10,),
-                    SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) => Icon(Icons.star, color: Colors.green,)),
+                      ),
+                     SizedBox(width: 10,),
+                      smalltext(text: "4.5"),
+                      SizedBox(width: 10,),
+                      smalltext(text: "12345"),
+                      SizedBox(width: 10,),
+                      smalltext(text: "comments"),
 
-                    Row(
-                      children: [
-                        Wrap(
-                          children: List.generate(5, (index) => Icon(Icons.star, color: Colors.green,)),
-                        ),
-                       SizedBox(width: 10,),
-                        smalltext(text: "4.5"),
-                        SizedBox(width: 10,),
-                        smalltext(text: "12345"),
-                        SizedBox(width: 10,),
-                        smalltext(text: "comments"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(icon: Icons.circle_sharp,
+                          text: "Normal",
 
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        IconAndTextWidget(icon: Icons.circle_sharp,
-                            text: "Normal",
+                          iconColor: Colors.blueGrey),
+                      IconAndTextWidget(icon: Icons.location_on,
+                          text: "1.7km",
 
-                            iconColor: Colors.blueGrey),
-                        IconAndTextWidget(icon: Icons.location_on,
-                            text: "1.7km",
+                          iconColor: Colors.blueGrey),
+                      IconAndTextWidget(icon: Icons.access_time_rounded,
+                          text: "32min",
 
-                            iconColor: Colors.blueGrey),
-                        IconAndTextWidget(icon: Icons.access_time_rounded,
-                            text: "32min",
+                          iconColor: Colors.blueGrey)
 
-                            iconColor: Colors.blueGrey)
-
-                      ],
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          )
-        ],
 
-      ),
+          ),
+        )
+      ],
+
     );
   }
 }
