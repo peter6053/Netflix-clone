6adbb7a4e33cb3396cb8f59e99fad5945bc3383e
buupass <buupass@buupasss-MacBook-Pro.local>
2022-03-26 21:06:34 +0300

    fixed alignment in pubspec

--- AndroidStudioProjects/untitled/lib/food_page_body.dart
+++ AndroidStudioProjects/untitled/lib/food_page_body.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+//import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:untitled/widgets/Big_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -9,6 +11,7 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
 
@@ -16,6 +19,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       color: Colors.yellow,
       height: 320,
       child: PageView.builder(
+            controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
         return _buildpageitem(position);
@@ -40,6 +44,34 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               )
 
 
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 150,
+            margin: EdgeInsets.only(left: 50, right: 40, bottom: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color:index.isEven? Colors.white: Colors.yellow,
+
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, right: 15, bottom: 15),
+              child: Column(
+                children: [
+                  BigText(text: "Chinese Side"),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) => Icon(Icons.star, color: Colors.green,)) ,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         )
       ],
